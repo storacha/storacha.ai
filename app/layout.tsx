9b8fc343a3ccd361b7acef2db22d6990b8079aea
Jay <jennifer.brawn@gmail.com>
2025-03-19 16:03:50 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { DM_Sans, DM_Mono } from 'next/font/google'
 import './globals.css'
-import Script from 'next/script'
 
 export const runtime = 'edge'
 
